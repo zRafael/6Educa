8a0fc0a5ab99c311d55ee07bb2aae60bb0ef7126
Israel <igarashiisrael@icloud.com>
2022-05-06 10:02:12 -0300

    Adicionando Repository, Controller e Relacionamento

--- 6Educa/src/main/java/com/generation/educa/Model/Tema.java
+++ 6Educa/src/main/java/com/generation/educa/Model/Tema.java
@@ -1,11 +1,17 @@
 package com.generation.educa.Model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "tb_tema")
@@ -32,6 +38,19 @@ public class Tema {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
+	@OneToMany(mappedBy="tema", cascade=CascadeType.ALL)
+	@JsonIgnoreProperties ("tema")
+	private List <Postagem> postagem;
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
 	
 	
 
